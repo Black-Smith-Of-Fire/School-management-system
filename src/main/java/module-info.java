07e440211eadd3ce538e6d5a478bce785f98f36a
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-08-12 09:28:49 +0530

    Changes in the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.mavenproject.school_management_system {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.mavenproject.school_management_system to javafx.fxml;
