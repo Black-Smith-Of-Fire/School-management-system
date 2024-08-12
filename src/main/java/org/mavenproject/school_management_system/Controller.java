0eaa224569434187513be88c987f278f268dd88b
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-08-13 01:15:09 +0530

    Testing to see if it would interact with the pageHandler class

--- src/main/java/org/mavenproject/school_management_system/Controller.java
+++ src/main/java/org/mavenproject/school_management_system/Controller.java
@@ -1,44 +1,49 @@
 package org.mavenproject.school_management_system;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
+
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
+
 public class Controller {
-//    private JButton student_pg, teacher_pg, courses_pg, transport_pg, classes_pg, fees_pg;
 
     @FXML
-    Stage stage;
+    private  Button student_pg, teacher_pg, courses_pg, transport_pg, classes_pg, fees_pg;
+    pageHandler pg = new pageHandler();
+
 
     @FXML
-    Scene scene;
+    protected void changePage(ActionEvent event) {
+        if (event.getSource() == student_pg){
+            pg.toPage("student");
+        }
 
-    @FXML
-    protected void changePage(ActionEvent event) throws IOException {
-//        student_pg = new JButton();
-
-        Parent root = FXMLLoader.load(getClass().getResource("dataPage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    protected void backPage(ActionEvent event) throws IOException {
-
-        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        if (event.getSource() == teacher_pg){
+            pg.toPage("teacher");
+        }
+        if (event.getSource() == courses_pg){
+            pg.toPage("courses");
+        }
+        if (event.getSource() == transport_pg){
+            pg.toPage("transport");
+        }
+        if (event.getSource() == classes_pg){
+            pg.toPage("classes");
+        }
+        if (event.getSource() == fees_pg){
+            pg.toPage("fees");
+        }
     }
 }
