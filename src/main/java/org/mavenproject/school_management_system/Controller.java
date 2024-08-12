a38fda528ed14f01e2cbe6be5b190d4e3aa658ee
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-08-12 09:30:11 +0530

    Syncing two pages together

--- src/main/java/org/mavenproject/school_management_system/Controller.java
+++ src/main/java/org/mavenproject/school_management_system/Controller.java
@@ -1,14 +1,44 @@
 package org.mavenproject.school_management_system;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+//    private JButton student_pg, teacher_pg, courses_pg, transport_pg, classes_pg, fees_pg;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Stage stage;
+
+    @FXML
+    Scene scene;
+
+    @FXML
+    protected void changePage(ActionEvent event) throws IOException {
+//        student_pg = new JButton();
+
+        Parent root = FXMLLoader.load(getClass().getResource("dataPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void backPage(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
