5cb3498f1f3f692d54af757059416c05fe8f55f5
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-08-24 20:39:32 +0530

    Removed function toPage as it is not able to communicate with the initializer function (forgot its spelling)

--- src/main/java/org/mavenproject/school_management_system/pageHandler.java
+++ src/main/java/org/mavenproject/school_management_system/pageHandler.java
@@ -1,51 +1,56 @@
 package org.mavenproject.school_management_system;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.*;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class pageHandler {
+public class pageHandler implements Initializable {
 
-//    @FXML
-//    private Stage stage;
-//
-//    @FXML
-//    private Scene scene ;
-//
-//    @FXML
-//    private Parent root;
+    @FXML
+    private Stage stage;
 
-//    @FXML
-//    public void sceneSwitch(ActionEvent event) throws IOException {
-//        root = FXMLLoader.load(getClass().getResource("dataPage.fxml"));
-//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
+    @FXML
+    private Scene scene;
 
-    public void toPage(String page){
-        if(page.equals("student")){
-            System.out.println("student");
-        }
-        else if (page.equals("teacher")) {
-            System.out.println("teacher");
-        }
-        else if (page.equals("courses")) {
-            System.out.println("courses");
-        }
-        else if (page.equals("transport")) {
-            System.out.println("transport");
-        }
-        else if (page.equals("fees")) {
-            System.out.println("fees");
-        }
-        else if (page.equals("classes")) {
-            System.out.println("classes");
-        }
+    @FXML
+    private Parent root;
+
+    @FXML
+    private ListView<String> myListView;
+
+    public String page = "";
+
+    Mysql mysql = new Mysql();
+
+    public void infoPage(ActionEvent event) throws  IOException {
+        root = FXMLLoader.load(getClass().getResource("personInfo.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        myListView.getItems().addAll(mysql.table(page));
+        myListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                System.out.println(myListView.getSelectionModel().getSelectedItem());
+            }
+        });
     }
 }
