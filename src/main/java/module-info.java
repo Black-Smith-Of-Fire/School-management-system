147ca34878bf8ab3570c2d3dc6e1bc62a5d6ef9c
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-08-24 20:35:06 +0530

    Stating the requirements of the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module org.mavenproject.school_management_system {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires mysql.connector.java;
+    requires java.sql;
 
 
     opens org.mavenproject.school_management_system to javafx.fxml;
