be6897e34d22570257f17133187c8b1f17a2e571
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-09-02 00:46:28 +0530

    Simple function , nothing new

--- src/main/java/org/mavenproject/school_management_system/Main.java
+++ src/main/java/org/mavenproject/school_management_system/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
