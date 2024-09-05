9e6f4d95eab5af78db6686cedb501aefdf67fd63
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-09-05 23:19:11 +0530

    Using vbox instead of listview

--- src/main/java/org/mavenproject/school_management_system/TeacherPage.java
+++ src/main/java/org/mavenproject/school_management_system/TeacherPage.java
@@ -1,80 +1,54 @@
 package org.mavenproject.school_management_system;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Duration;
-
-import java.awt.event.MouseEvent;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class TeacherPage implements Initializable {
 
     @FXML
-    private ListView<String> teacherList;
+    private VBox vbox;
 
     @FXML
     private AnchorPane pane1;
 
-
     Mysql mysql = new Mysql();
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-//        pane1.setTranslateX(240);
-        teacherList.getItems().addAll(mysql.table("teachers"));
+    public void initialize(URL location, ResourceBundle resources) { // Should load the data into the vbox
+
+        FXMLLoader loader = new FXMLLoader();
+
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(mysql.table("teachers"));
+        for (int i = 0; i < list.size(); i++) { // testing arraylist
+            System.out.println(list.get(i));
+        }
+
+        try{
+            Node[] node = new Node[5];
+
+            for (int i = 0; i < node.length; i++) {
+                node[i] = FXMLLoader.load(getClass().getResource("item.fxml"));
+                vbox.getChildren().add(node[i]); // Supposed to add items in a vbox
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     @FXML
     private void menu(){
         System.out.println("menu working");
-//        pane1.setVisible(true);
-//        TranslateTransition slide = new TranslateTransition();
-//        slide.setDuration(Duration.ONE);
-//        slide.setNode(pane1);
-//        slide.setToX(0); //200
-//        slide.play();
-//        pane1.setTranslateX(240);
-
-//        TranslateTransition slide = new TranslateTransition();
-//        slide.setDuration(Duration.seconds(0.4));
-//        slide.setNode(pane1);
-//
-//        slide.setToX(0);
-//        slide.play();
-
-//        pane1.setTranslateX(160);
-
-//        slide.setOnFinished((ActionEvent event) -> {
-//
-//        });
-    }
-    /*
-
-    @FXML
-    private AnchorPane panelslide;
-
-    @FXML
-    private void run() { // function to slide the navbar
-        TranslateTransition slide = new TranslateTransition();
-        slide.setDuration(Duration.ONE);
-        slide.setNode(panelslide);
-        slide.setToX(0); //200
-        slide.play();
-        panelslide.setTranslateX(200);
 
     }
 
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        panelslide.setTranslateX(200);
     }
-    * */
-}
