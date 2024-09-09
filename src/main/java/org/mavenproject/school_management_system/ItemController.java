d8e72f57c8dac2f44554a4392c8081bebb8b8e9a
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-09-09 12:46:35 +0530

    For now this will just change the info

--- src/main/java/org/mavenproject/school_management_system/ItemController.java
+++ src/main/java/org/mavenproject/school_management_system/ItemController.java
@@ -1,4 +1,22 @@
 package org.mavenproject.school_management_system;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class ItemController {
+
+    @FXML
+    ImageView imageView;
+
+    @FXML
+    Label darth_label; // to be edited
+
+//    @FXML
+//    Label registration_number; // For the future
+
+public void setItemInfo(String name  /*, String image*/ ) {
+    darth_label.setText(name);
+
+}
 }
